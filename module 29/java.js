d15fae22a3b45447548f817a56f4e7489e6925f3
ledionh10 <ledionhalimi8@gmail.com>
2025-09-15 17:43:27 +0200

    hsdfhs

---
+++ module 29/java.js
@@ -0,0 +1,18 @@
+var Programminglanguages = ['JAVA','PHP','Python'];
+
+console.log(Programminglanguages[0]);
+console.log(Programminglanguages[1]);
+console.log(Programminglanguages[2]);
+
+
+Programminglanguages.push('Javascript');
+console.log(Programminglanguages[3]);
+
+Programminglanguages.pop();
+
+Programminglanguages.unshift('JavaScript');
+console.log(Programminglanguages);
+
+Programminglanguages.shift();
+
+Programminglanguages.slice(0,1, Ruby);
