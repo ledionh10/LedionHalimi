4b0df6564540a81fcb2d37384fd7c691be5242ad
ledionh10 <ledionhalimi8@gmail.com>
2025-09-15 17:14:00 +0200

    gjasngklasbopg

---
+++ module 28/shapes challenge/java.js
@@ -0,0 +1,21 @@
+var v_button = document.getElementById("btn1");
+var v_button2 = document.getElementById("btn2");
+var v_button3 = document.getElementById("btn3");
+
+
+
+
+
+
+v_button.onclick= function(){
+    v_button.setAttribute("class", "btn1");
+}
+
+v_button2.onclick= function(){
+    v_button2.setAttribute("class", "btn2");
+}
+
+v_button3.onclick= function(){
+    v_button3.setAttribute("class", "btn3");
+}
+
