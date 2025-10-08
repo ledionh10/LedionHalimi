50eeedd3cc5b7a1fc8c4aba9d3bb26af5398844b
ledionh10 <ledionhalimi8@gmail.com>
2025-10-08 17:44:22 +0200

    module 36

---
+++ Module 36/js/main.js
@@ -0,0 +1,16 @@
+$(window).ready(function(){
+    alert('It is working');
+});
+
+$('#header');
+$('li');
+$('ul,li');
+$('.student');
+
+$('a.test:first');
+$('tr:odd');
+$('#myForm :input');
+$('div:visible');
+$('div:gt(2)');
+$('div:animated');
+  
