150839dcf8d5a826d3946d1390e802d8833f2ef4
ledionh10 <ledionhalimi8@gmail.com>
2025-10-20 17:40:20 +0200

    lesson

---
+++ module 37/js/main.js
@@ -0,0 +1,20 @@
+$('#sort').sortable();
+
+$(function(){
+	$("#accordion").accordion();
+});
+
+$('#tabs').tabs();
+
+$(function(){
+	$("#accordion").accordion({
+		collapsible: true
+	});
+});
+
+$(function(){
+	$("#tabs").tabs({
+		collapsible: true
+	});
+});
+
